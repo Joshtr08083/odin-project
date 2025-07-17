b1923838aa91c826cf431883d26c0f6b506a8344
Joshtr08083 <joshtreid08@gmail.com>
2025-07-17 12:32:09 -0400

    added animations and transitions

---
+++ HTML-Intro/scripts/transition.js
@@ -0,0 +1,36 @@
+// Scripts arent part of html intro (and really neither is css), they are just here for decor
+
+let transitionData
+try {
+    transitionData = JSON.parse(sessionStorage.getItem("transition"));
+} catch {
+
+}
+
+const transitionDelay = 500;
+const transitionElement = document.getElementById("transition");
+
+if (transitionData && transitionData.bool) {
+    // skip transition if origin is sae, this is to prevent it running every reload
+    if (transitionData.origin !== window.location.href) {
+        document.referrer = "";
+        transitionElement.classList.add("transitionEnd");
+    
+        setTimeout(() => {
+            transitionElement.classList.remove("transitionEnd")
+        }, transitionDelay);
+    }
+    
+    sessionStorage.removeItem("transition");
+
+}
+
+
+function transition(target) {
+    transitionElement.classList.add("transitionStart");
+    sessionStorage.setItem("transition", "{\"bool\":true, \"origin\":\"" + window.location.href + "\"}");
+    
+    setTimeout(() => {
+        document.location.replace(target);
+    }, transitionDelay);
+}
